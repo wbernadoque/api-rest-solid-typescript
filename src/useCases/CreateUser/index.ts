82d613908c546b0a11d33f96fc5d4f808fbae576
Wesley <wnbeq@hotmail.com>
2023-04-06 09:52:33 -0300

    first commit

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,16 @@
+import { MailtrapMailProvider } from '../../providers/implementations/MailtrapMailProvider';
+import { PostgresUsersRepository } from '../../repositories/implementations/PostgresUsersRepository';
+import { CreateUserController } from './CreateUserController';
+import { CreateUserUseCase } from './CreatreUserUseCase';
+
+const mailtrapProvider = new MailtrapMailProvider();
+const postgresUsersRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+  postgresUsersRepository,
+  mailtrapProvider
+);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+export { createUserUseCase, createUserController };
